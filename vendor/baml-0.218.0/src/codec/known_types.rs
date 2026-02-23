30d7f549bcae2a81bc358f07a2b9565f3ff04040
Samuel Galanakis <samme.galanakis6@gmail.com>
2026-02-23 21:14:09 +0100

    remove remaining BAML artifacts

--- vendor/baml-0.218.0/src/codec/known_types.rs
+++
@@ -1,6 +0,0 @@
-//! Re-export `KnownTypes` from crate root.
-//!
-//! The actual trait definition is in `crate::known_types` to avoid
-//! circular dependencies between error.rs and codec/.
-
-pub use crate::known_types::KnownTypes;
